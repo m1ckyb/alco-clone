40cd6a896deabe9b64d7e388f55fe9a008fb22b4
Michael <michael@micky.id.au>
2026-06-06 23:00:45 +1000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/alco-clone/sw.js', { scope: '/alco-clone/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sipwise/sw.js', { scope: '/sipwise/' })})}
